48e71ad457bf0b4cd4df41210b1e2bf5d96916ce
Alexander <ana2023@uw.edu>
2022-02-24 16:56:49 -0800

    Implemented CampusMap

--- hw-pathfinder/src/main/java/pathfinder/CampusMap.java
+++ hw-pathfinder/src/main/java/pathfinder/CampusMap.java
@@ -11,35 +11,80 @@
 
 package pathfinder;
 
+import graph.Graph;
 import pathfinder.datastructures.Path;
 import pathfinder.datastructures.Point;
+import pathfinder.parser.CampusBuilding;
+import pathfinder.parser.CampusPath;
+import pathfinder.parser.CampusPathsParser;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CampusMap implements ModelAPI {
+    private final Graph<Point, Double> campusMap;
+    private final Map<String, CampusBuilding> campusBuildings;
+
+    public CampusMap() { // +=+ Can a non-ADT have a constructor?
+        campusMap = new Graph<>();
+        campusBuildings = new HashMap<>();
+
+        List<CampusBuilding> buildings = CampusPathsParser.parseCampusBuildings("campus_buildings.csv");
+        List<CampusPath> paths = CampusPathsParser.parseCampusPaths("campus_paths.csv");
+
+        for (CampusBuilding building : buildings) {
+            campusBuildings.put(building.getShortName(), building);
+        }
+
+        for (CampusPath path : paths) {
+            Point src = new Point(path.getX1(), path.getY1());
+            Point dst = new Point(path.getX2(), path.getY2());
+
+            campusMap.addNode(src);
+            campusMap.addNode(dst);
+
+            campusMap.addEdge(path.getDistance(), src, dst);
+        }
+    }
 
     @Override
     public boolean shortNameExists(String shortName) {
-        // TODO: Implement this method exactly as it is specified in ModelAPI
-        throw new RuntimeException("Not Implemented Yet");
+        return campusBuildings.containsKey(shortName);
     }
 
     @Override
     public String longNameForShort(String shortName) {
-        // TODO: Implement this method exactly as it is specified in ModelAPI
-        throw new RuntimeException("Not Implemented Yet");
+        if (!shortNameExists(shortName)) {
+            throw new IllegalArgumentException();
+        }
+
+        return campusBuildings.get(shortName).getLongName();
     }
 
     @Override
     public Map<String, String> buildingNames() {
-        // TODO: Implement this method exactly as it is specified in ModelAPI
-        throw new RuntimeException("Not Implemented Yet");
+        Map<String, String> names = new HashMap<>();
+
+        for (String shortName : campusBuildings.keySet()) {
+            names.put(shortName, campusBuildings.get(shortName).getLongName());
+        }
+
+        return names;
     }
 
     @Override
     public Path<Point> findShortestPath(String startShortName, String endShortName) {
-        // TODO: Implement this method exactly as it is specified in ModelAPI
-        throw new RuntimeException("Not Implemented Yet");
-    }
+        if (startShortName == null || endShortName == null ||
+            !shortNameExists(startShortName) || !shortNameExists(endShortName)) {
+            throw new IllegalArgumentException();
+        }
 
+        Point src = new Point(campusBuildings.get(startShortName).getX(),
+                              campusBuildings.get(startShortName).getY());
+        Point dst = new Point(campusBuildings.get(endShortName).getX(),
+                              campusBuildings.get(endShortName).getY());
+
+        return PathFinderPaths.findPath(campusMap, src, dst);
+    }
 }
